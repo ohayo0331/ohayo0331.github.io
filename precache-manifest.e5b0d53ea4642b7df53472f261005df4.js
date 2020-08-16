50cc1c74f95d39845ae19c7aedf3da87220a096d
Hangyi Yang <yanghenr@usc.edu>
2020-08-16 16:21:51 -0700

    Updates

---
+++ precache-manifest.e5b0d53ea4642b7df53472f261005df4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e13d8c5cb7ed23f5892a3c5c0160010",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e66878cbe55e6ab68c24",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "0b96d6ca684ddf638358",
+    "url": "/static/js/2.3542fd3c.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3542fd3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e66878cbe55e6ab68c24",
+    "url": "/static/js/main.08a0d393.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
