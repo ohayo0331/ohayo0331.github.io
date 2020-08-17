7b7659ee7961beaeb49587ab5c7df44ea1e92913
Hangyi Yang <yanghenr@usc.edu>
2020-08-16 22:59:18 -0700

    Updates

---
+++ precache-manifest.1c9725c37f1f604984ded3f89f75a8b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "608774f378a18f9a4e2b92ecf101abc4",
+    "url": "/laughtale.me/index.html"
+  },
+  {
+    "revision": "11a2f55e1aa97d88e541",
+    "url": "/laughtale.me/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "0b96d6ca684ddf638358",
+    "url": "/laughtale.me/static/js/2.3542fd3c.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/laughtale.me/static/js/2.3542fd3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11a2f55e1aa97d88e541",
+    "url": "/laughtale.me/static/js/main.ef875384.chunk.js"
+  },
+  {
+    "revision": "fc2ddfd538802a81b7ec",
+    "url": "/laughtale.me/static/js/runtime-main.36aae206.js"
+  }
+]);
